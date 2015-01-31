12a1897a8f3be51341c4a4acfcc65f7dcd7d3423
gideonparanoid <gideon.jones@ntlworld.com>
2015-01-31 00:53:29 +0000

    little additions

--- src/mmp/app/src/main/java/uk/ac/aber/gij2/mmp/visualisation/Component.java
+++ src/mmp/app/src/main/java/uk/ac/aber/gij2/mmp/visualisation/Component.java
@@ -15,6 +15,8 @@ public class Component extends Shape {
 
 
    private final double angle = Math.PI / 4d;
+   private double length = 1;
+   private  int pitch, yaw, roll;
 
    /**
     *
@@ -26,10 +28,14 @@ public class Component extends Shape {
    public Component(int program, int pitch, int yaw, int roll) {
       super(program);
 
+      this.pitch = pitch;
+      this.yaw = yaw;
+      this.roll = roll;
+
       double directionAngle = Math.atan((double) pitch / (double) yaw),
-         x = Math.sin(angle) * Math.cos(directionAngle),
-         y = Math.sin(angle) * Math.sin(directionAngle),
-         z = pitch == ZERO && yaw == ZERO ? 1d : Math.cos(angle);
+         x = length * Math.sin(angle) * length * Math.cos(directionAngle),
+         y = length * Math.sin(angle) * length * Math.sin(directionAngle),
+         z = pitch == ZERO && yaw == ZERO ? length : length * Math.cos(angle);
 
 
       setVertexCoords(new float[]{
@@ -46,5 +52,22 @@ public class Component extends Shape {
       });
 
       setup();
+   }
+
+
+   public int getPitch() {
+      return pitch;
+   }
+
+   public int getRoll() {
+      return roll;
+   }
+
+   public int getYaw() {
+      return yaw;
+   }
+
+   public double getLength() {
+      return length;
    }
 }
