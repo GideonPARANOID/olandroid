3adefc9282c70454ba39229a1e7d345f473714ec
gideonparanoid <gideon.jones@ntlworld.com>
2015-04-28 13:03:22 +0100

    more manoeuvres, with diagrams, properly integrated

--- res/manoeuvre-svg-render.sh
+++ res/manoeuvre-svg-render.sh
@@ -6,7 +6,8 @@ grep -rl "802" manoeuvre-svg | xargs sed -i 's/802/162/g'
 mkdir manoeuvre-png
 
 # rendering
-for i in *svg; do
-   inkscape -z -e ${i//svg/png} -w 486 -h 780 $i
+for i in manoeuvre-svg/*svg; do
+   out=${i//svg/png}
+   inkscape -z -e manoeuvre-png/manoeuvre_${out##*/} -w 486 -h 780 $i
 done
 
